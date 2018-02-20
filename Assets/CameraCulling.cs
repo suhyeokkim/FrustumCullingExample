dd6db52b9d3550e4693f3599a8cfe89bb5ae1191
zizon <tngurdl00@hanmail.net>
2018-02-21 00:27:59 +0900

    Add all

---
+++ Assets/CameraCulling.cs
@@ -0,0 +1,156 @@
+﻿namespace Safari
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public enum Caculation
+    {
+        Auto,
+        ComputeShader,
+        Script,
+    }
+
+    [RequireComponent(typeof(Camera))]
+    public class CameraCulling : MonoBehaviour
+    {
+        public new Camera camera;
+
+        [SerializeField]
+        public ComputeShader cullingComputeShader;
+        
+        [SerializeField]
+        public Renderer[] staticRenderers;
+
+        [NonSerialized]
+        public int boundCullHash;
+
+        [NonSerialized]
+        public uint threadSizeX;
+
+        [SerializeField]
+        public Bounds[] staticRendererBounds;
+        public ComputeBuffer boundGPUBuffer;
+
+        [NonSerialized]
+        public int[] cullingResults;
+        public ComputeBuffer resultGPUBuffer;
+
+        [NonSerialized]
+        public Plane[] frustomPlanes;
+        public ComputeBuffer planeGPUBUffer;
+
+        [ContextMenu("Get static renderers")]
+        private void GetStaticRenderers()
+        {
+            staticRenderers = Array.FindAll(FindObjectsOfType<Renderer>(), (renderer) => renderer.gameObject.isStatic);
+            staticRendererBounds = Array.ConvertAll(staticRenderers, (renderer) => renderer.bounds);
+
+            Debug.Log(staticRendererBounds.Length);
+        }
+
+        [ContextMenu("Get camera")]
+        private void GetCamera()
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        private void Init()
+        {
+            boundCullHash = cullingComputeShader.FindKernel("OverlapFrustomAndBounds");
+
+            if (boundCullHash < 0)
+            {
+                Debug.LogError("CameraCulling.Init >> frustom and bound kernel not exist..");
+            }
+
+            uint threadSizeY;
+            uint threadSizeZ;
+
+            cullingComputeShader.GetKernelThreadGroupSizes(boundCullHash, out threadSizeX, out threadSizeY, out threadSizeZ);
+
+            if (staticRendererBounds == null)
+                staticRendererBounds = Array.ConvertAll(staticRenderers, (renderer) => renderer.bounds);
+
+            frustomPlanes = new Plane[6];
+            planeGPUBUffer = new ComputeBuffer(6, Marshal.SizeOf(typeof(Plane)));
+            planeGPUBUffer.SetData(frustomPlanes);
+
+            boundGPUBuffer = new ComputeBuffer(staticRenderers.Length, Marshal.SizeOf(typeof(Bounds)));
+            boundGPUBuffer.SetData(staticRendererBounds);
+
+            if (cullingResults == null)
+                cullingResults = new int[staticRenderers.Length];
+
+            resultGPUBuffer = new ComputeBuffer(staticRenderers.Length, Marshal.SizeOf(typeof(uint)));
+            resultGPUBuffer.SetData(cullingResults);
+
+            cullingComputeShader.SetInt("resultLength", staticRenderers.Length);
+
+            cullingComputeShader.SetBuffer(boundCullHash, "bounds", boundGPUBuffer);
+            cullingComputeShader.SetBuffer(boundCullHash, "planes", planeGPUBUffer);
+            cullingComputeShader.SetBuffer(boundCullHash, "results", resultGPUBuffer);
+        }
+
+        public void Cull()
+        {
+            GeometryUtility.CalculateFrustumPlanes(camera, frustomPlanes);
+            planeGPUBUffer.SetData(frustomPlanes);
+
+            uint length = (uint)staticRenderers.Length;
+            uint dispatchXLength = length / threadSizeX + (uint)(((int)length % (int)threadSizeX > 0) ? 1 : 0);
+
+            cullingComputeShader.Dispatch(boundCullHash, (int)dispatchXLength, 1, 1);
+            resultGPUBuffer.GetData(cullingResults);
+
+            /// 1000 내외까지 가능
+            for (int i = 0; i < staticRenderers.Length; i++)
+                staticRenderers[i].enabled = cullingResults[i] != 0;
+        }
+
+        private void Awake()
+        {
+            Init();
+
+            prevPos = camera.transform.position;
+            prevRot = camera.transform.rotation;
+
+            Cull();
+        }
+
+        private Vector3 prevPos;
+        private Quaternion prevRot;
+
+        private void OnPreCull()
+        {
+            if (prevPos != camera.transform.position || prevRot != camera.transform.rotation)
+            {
+                prevPos = camera.transform.position;
+                prevRot = camera.transform.rotation;
+
+                Cull();
+            }
+        }
+
+        public float moveValue = 5f;
+
+        public void Update()
+        {
+            Vector3 rotation = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+                rotation.x -= moveValue * Time.deltaTime;
+            else if (Input.GetKey(KeyCode.S))
+                rotation.x += moveValue * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.A))
+                rotation.y -= moveValue * Time.deltaTime;
+            else if (Input.GetKey(KeyCode.D))
+                rotation.y += moveValue * Time.deltaTime;
+
+            transform.Rotate(rotation);
+        }
+    }
+}
---
+++ Assets/ObjectGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGenerator : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject source;
+
+    [SerializeField]
+    public Vector3Int genCount;
+
+    [SerializeField]
+    public Vector3 gap;
+
+    [ContextMenu("Generate objects")]
+    public void Generate()
+    {
+        Vector3 start = new Vector3(genCount.x * gap.x, genCount.y * gap.y, genCount.z * gap.z) / 2;
+
+        for (int x = 0; x < genCount.x; x++)
+        {
+            for (int y = 0; y < genCount.y; y++)
+            {
+                for (int z = 0; z < genCount.z; z++)
+                {
+                    GameObject copied = Instantiate(source);
+
+                    copied.name = string.Format("{0},{1},{2}", x, y, z);
+                    copied.transform.position = new Vector3(gap.x * x, gap.y * y, gap.z * z) - start;
+
+                    copied.transform.parent = transform;
+                }
+            }
+        }
+    }
+
+    [ContextMenu("Clear objects")]
+    public void Clear()
+    {
+        while(transform.childCount > 0)
+            DestroyImmediate(transform.GetChild(0).gameObject);
+    }
+}
